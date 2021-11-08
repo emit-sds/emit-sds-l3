de690e429034c2b41ebb63d7427a6b0b5857fc34
brodrick <phil.brodrick@gmail.com>
2021-11-08 15:56:45 -0800

    cleanup args and adapt for single line instances

--- build_mosaic_glt.jl
+++ build_mosaic_glt.jl
@@ -1,5 +1,5 @@
 using ArchGDAL
-using ArgParse
+using ArgParse2
 using EllipsisNotation
 using DelimitedFiles
 using Logging
@@ -10,68 +10,49 @@ using Statistics
 function main()
 
     s = ArgParseSettings()
-    @add_arg_table s begin
-        "output_filename"
-            help = "File to write results to"
-            arg_type = String
-            required = true
-        "igm_file_list"
-            help = "File to write results to"
-            arg_type = String
-            required = true
-        "target_resolution"
-            help = "GSD to build the mosaic at"
-            arg_type = Float64
-            nargs = 2
-            required = true
-        "--criteria_mode"
-            help= "Band-ordering criteria mode.  Options are min or max (require criteria file), or distance (uses closest point)"
-            arg_type = String
-            required = false
-            default = "distance"
-        "--criteria_band"
-            help = "band of criteria file list to use"
-            arg_type = Int64
-            required = false
-            default = 1
-        "--criteria_file_list"
-            help = "files used for criteria"
-            arg_type = Float64
-            required = false
-        "--target_extent_ul_lr"
-            help = "extent to build the mosaic of"
-            arg_type = Float64
-            nargs = 4
-            required = false
-        "--log_file"
-            help = "log file to write to"
-            arg_type = String
-            required = false
-            default = nothing
-    end
 
-    args = parse_args(ARGS, s)
-    target_resolution = args["target_resolution"]
+    parser = ArgumentParser(prog = "GLT Builder",
+                        description = "Build GLTs from one or more files")
 
-    if isnothing(args["log_file"])
+    add_argument!(parser, "output_filename", type = String, help = "File to write GLT results to")
+    add_argument!(parser, "igm_file_list", type = String, help = "IGM file or list of files to build GLT from")
+    add_argument!(parser, "target_resolution", type = Float64, nargs=2, help = "GSD (x and y).")
+    add_argument!(parser, "--criteria_mode", type = String, default = "distance", help = "Band-ordering criteria mode.  Options are min or max (require criteria file), or distance (uses closest point)")
+    add_argument!(parser, "--criteria_band", type = Int64, default = 1, help = "band of criteria file to use")
+    add_argument!(parser, "--criteria_file_list", type = String, help = "file(s) to be used for criteria")
+    add_argument!(parser, "--target_extent_ul_lr", type = Float64, nargs=4, help = "extent to build the mosaic of")
+    add_argument!(parser, "--mosaic", type = Int32, default=1, help = "treat as a mosaic")
+    add_argument!(parser, "--log_file", type = String, default = nothing, help = "log file to write to")
+    args = parse_args(parser)
+
+    if isnothing(args.log_file)
         logger = Logging.SimpleLogger()
     else
-        logger = Logging.SimpleLogger(args["log_file"])
+        logger = Logging.SimpleLogger(args.log_file)
     end
     Logging.global_logger(logger)
 
-    #if ! (args["criteria_mode"] ! in ["min","max","distance"])
+    #if ! (args.criteria_mode ! in ["min","max","distance"])
     #    error("Invalid criteria_mode, expected on of min, max, distance")
     #end
 
-    igm_files = readdlm(args["igm_file_list"], String)
-    if args["criteria_mode"] != "distance"
-        criteria_files = readdlm(args["criteria_file_list"], String)
+    if args.mosaic == 1
+        igm_files = readdlm(args.igm_file_list, String)
+    else
+        igm_files = [args.igm_file_list]
+    end
+
+    if args.criteria_mode != "distance"
+        if args.mosaic == 1
+            criteria_files = readdlm(args.criteria_file_list, String)
+        else
+            criteria_files = [args.criteria_file_list]
+        end
         # TODO: add check to make sure criteria file dimensions match igm file dimensions
     end
 
-    if length(args["target_extent_ul_lr"]) > 0
-        ullr = args["target_extent_ul_lr"]
+    if length(args.target_extent_ul_lr) > 0
+        ullr = args.target_extent_ul_lr
         min_x = ullr[1]
         max_y = ullr[2]
         max_x = ullr[3]
@@ -82,32 +63,37 @@ function main()
     @info "IGM bounds: $min_x, $max_y, $max_x, $min_y"
 
     @info "Tap to a regular Grid"
-    min_x = tap_bounds(min_x, target_resolution[1], "down")
-    max_y = tap_bounds(max_y, target_resolution[2], "up")
-    max_x = tap_bounds(max_x, target_resolution[1], "up")
-    min_y = tap_bounds(min_y, target_resolution[2], "down")
+    min_x = tap_bounds(min_x, args.target_resolution[1], "down")
+    max_y = tap_bounds(max_y, args.target_resolution[2], "up")
+    max_x = tap_bounds(max_x, args.target_resolution[1], "up")
+    min_y = tap_bounds(min_y, args.target_resolution[2], "down")
 
     @info "Tapped bounds: $min_x, $max_y, $max_x, $min_y"
 
-    x_size_px = Int32(ceil((max_x - min_x) / target_resolution[1]))
-    y_size_px = Int32(ceil((max_y - min_y) / -target_resolution[2]))
+    x_size_px = Int32(ceil((max_x - min_x) / args.target_resolution[1]))
+    y_size_px = Int32(ceil((max_y - min_y) / -args.target_resolution[2]))
 
     @info "Output Image Size (x,y): $x_size_px, $y_size_px.  Creating output dataset."
-    outDataset = ArchGDAL.create(args["output_filename"], driver=ArchGDAL.getdriver("ENVI"), width=x_size_px,
+    if args.mosaic == 1
+        output_bands = 3
+    else
+        output_bands = 2
+    end
+    outDataset = ArchGDAL.create(args.output_filename, driver=ArchGDAL.getdriver("ENVI"), width=x_size_px,
     height=y_size_px, nbands=3, dtype=Float32)
     ArchGDAL.setproj!(outDataset, ArchGDAL.toWKT(ArchGDAL.importEPSG(4326)))
-    ArchGDAL.setgeotransform!(outDataset, [min_x, target_resolution[1], 0, max_y, 0, target_resolution[2]])
+    ArchGDAL.setgeotransform!(outDataset, [min_x, args.target_resolution[1], 0, max_y, 0, args.target_resolution[2]])
 
     @info "Populate target grid."
     grid = Array{Float64}(undef, y_size_px, x_size_px, 2)
-    grid[..,1] = fill(1,y_size_px,x_size_px) .* LinRange(min_x + target_resolution[1]/2,min_x + target_resolution[1] * (1/2 + x_size_px - 1), x_size_px)[[CartesianIndex()],:]
-    grid[..,2] = fill(1,y_size_px,x_size_px) .* LinRange(max_y + target_resolution[2]/2,max_y + target_resolution[2] * (1/2 + y_size_px - 1), y_size_px)[:,[CartesianIndex()]]
+    grid[..,1] = fill(1,y_size_px,x_size_px) .* LinRange(min_x + args.target_resolution[1]/2,min_x + args.target_resolution[1] * (1/2 + x_size_px - 1), x_size_px)[[CartesianIndex()],:]
+    grid[..,2] = fill(1,y_size_px,x_size_px) .* LinRange(max_y + args.target_resolution[2]/2,max_y + args.target_resolution[2] * (1/2 + y_size_px - 1), y_size_px)[:,[CartesianIndex()]]
 
     @info "Create GLT."
     best = fill(1e12, y_size_px, x_size_px, 4)
     best[..,1:3] .= -9999
 
-    max_offset_distance = sqrt(sum(target_resolution.^2))*3
+    max_offset_distance = sqrt(sum(args.target_resolution.^2))*3
     pixel_buffer_window = 1
 
     total_found = 0
@@ -123,15 +109,15 @@ function main()
         else
             println("Entering")
         end
-        if args["criteria_mode"] != "distance"
+        if args.criteria_mode != "distance"
             criteria_dataset = ArchGDAL.read(criteria_files[file_idx])
-            criteria = PermutedDimsArray(ArchGDAL.read(criteria_dataset, args["criteria_band"]), (2,1))
+            criteria = PermutedDimsArray(ArchGDAL.read(criteria_dataset, args.criteria_band), (2,1))
         end
         for _y=1:size(igm)[1]
             for _x=1:size(igm)[2]
                 pt = igm[_y,_x,1:2]
-                closest_t = Array{Int64}([round((pt[2] - grid[1,1,2]) / target_resolution[2]), 
-                                        round((pt[1] - grid[1,1,1]) / target_resolution[1])  ]) .+ 1
+                closest_t = Array{Int64}([round((pt[2] - grid[1,1,2]) / args.target_resolution[2]),
+                                        round((pt[1] - grid[1,1,1]) / args.target_resolution[1])  ]) .+ 1
 
                 closest = zeros(Int64,2)
                 for xbuffer in -pixel_buffer_window:pixel_buffer_window
@@ -147,8 +133,8 @@ function main()
 
                         if dist < max_offset_distance
 
-                            if args["criteria_mode"] in ["distance", "min"]
-                                if args["criteria_mode"] == "distance"
+                            if args.criteria_mode in ["distance", "min"]
+                                if args.criteria_mode == "distance"
                                     current_crit = dist
                                 else
                                     current_crit = criteria[closest[1], closest[2]]
@@ -158,7 +144,7 @@ function main()
                                     best[closest[1], closest[2], 1:3] = [_x, _y, file_idx]
                                     best[closest[1], closest[2], 4] = current_crit
                                 end
-                            elseif args["criteria_mode"] == "max"
+                            elseif args.criteria_mode == "max"
                                 current_crit = criteria[closest[1], closest[2]]
                                 if current_crit > best[closest[1], closest[2], 4]
                                     best[closest[1], closest[2], 1:3] = [_x, _y, file_idx]
@@ -174,7 +160,11 @@ function main()
     end
 
     println(total_found, " ", sum(best[..,1] .!= -9999), " ", size(best)[1]*size(best)[2])
-    output = Array{Int32}(permutedims(best[..,1:3], (2,1,3)))
+    if args.mosaic == 1
+        output = Array{Int32}(permutedims(best[..,1:3], (2,1,3)))
+    else
+        output = Array{Int32}(permutedims(best[..,1:2], (2,1,3)))
+    end
     ArchGDAL.write!(outDataset, output, [1:size(output)[end];], 0, 0, size(output)[1], size(output)[2])
 
 end
