9caa93ac0f36ef88b95ea293c20290c5df775da4
brodrick <phil.brodrick@gmail.com>
2024-02-14 07:54:20 -0800

    add count fraction option

--- veg_correction.py
+++ veg_correction.py
@@ -42,6 +42,7 @@ def main(input_args=None):
     parser.add_argument('--out_file', type=str, default=None)   
     parser.add_argument('--soil_thresh', type=float, default=0.001)   
     parser.add_argument('--coarsened_file', type=str, default=None)   
+    parser.add_argument('--mask_fraction_file', type=str, default=None)   
     parser.add_argument('--resolution', type=float, default=None)   
     parser.add_argument('--data_threshold', type=float, default=None)   
     parser.add_argument('--abun_uncert_file', type=str, default=None)   
@@ -59,14 +60,22 @@ def main(input_args=None):
 
     abun = abun_ds.open_memmap(interleave='bip').copy()
     cover = cover_ds.open_memmap(interleave='bip')[...,2].copy()
+    counts = {}
+
     if args.thresh_only is False:
         abun = abun / cover[:,:,np.newaxis]
+
+    counts['no_abun'] = np.any(np.logical_or.reduce((np.isnan(abun) , np.isfinite(abun) == False, abun == -9999)),axis=-1)
     masked_out = np.any(np.isnan(abun), axis=-1)
     masked_out[np.any(np.isfinite(abun) == False,axis=-1)] = True
-    masked_out[cover < args.soil_thresh] = True
     masked_out[np.any(abun == -9999,axis=-1)] = True
+
+    counts['soil_cutoff'] = np.logical_and(masked_out == False, cover < args.soil_thresh)
+    masked_out[cover < args.soil_thresh] = True
+
     if args.mask_file is not None:
         ext_mask = gdal.Open(args.mask_file).ReadAsArray()
+        counts['external_mask'] = np.logical_and(masked_out == False, ext_mask == 1)
         masked_out[ext_mask == 1] = True
 
     abun[masked_out,:] = -9999
@@ -85,7 +94,6 @@ def main(input_args=None):
         abununcert[masked_out,:] = -9999
         coveruncert[masked_out] = -9999
         do_uncert = True
-
 
 
     # Build output dataset
@@ -117,6 +125,7 @@ def main(input_args=None):
         numy = int(round(abun.shape[0] / num_px))
         numx = int(round(abun.shape[1] / num_px))
         asa = np.zeros((numy, numx,abun.shape[2])) - 9999
+        agg_count = np.zeros((numy, numx,len(counts.keys()))) - 9999
         asa_unc = None
         if do_uncert:
             asa_unc = np.zeros((numy, numx,abun.shape[2])) - 9999
@@ -129,6 +138,8 @@ def main(input_args=None):
                     if complete_frac < args.data_threshold:
                         continue
                 asa[_y,_x,:] = np.nanmean(abun[_y*num_px:(_y+1)*num_px,_x*num_px:(_x+1)*num_px,:],axis=(0,1))
+                for _key, key in enumerate(counts.keys()):
+                    agg_count[_y,_x,_key] = np.sum(counts[key][_y*num_px:(_y+1)*num_px,_x*num_px:(_x+1)*num_px]) / np.product(abun[_y*num_px:(_y+1)*num_px,_x*num_px:(_x+1)*num_px,0].shape)
 
                 if do_uncert:
                     valid_unc = abununcert[_y*num_px:(_y+1)*num_px,_x*num_px:(_x+1)*num_px,:]
@@ -144,6 +155,7 @@ def main(input_args=None):
 
                     asa_unc[_y,_x,:] = np.sqrt(np.power(asa[_y,_x,:] / valid_px,2) * np.nansum(inner_term,axis=0))
 
+        # Spectral Abundance
         outDataset = driver.Create(args.coarsened_file, asa.shape[1], asa.shape[0],
                                    asa.shape[2], gdal.GDT_Float32, options=['INTERLEAVE=BIL'])
         outDataset.SetProjection(abun_gdal.GetProjection())
@@ -157,6 +169,23 @@ def main(input_args=None):
                 outDataset.GetRasterBand(_b).SetDescription(band_names[_b-1])
         del outDataset
         _write_bil_chunk(asa.transpose((0,2,1)), args.coarsened_file, 0, (asa.shape[0], asa.shape[2], asa.shape[1]))
+
+
+        # Count fractions
+        if args.mask_fraction_file is not None:
+            outDataset = driver.Create(args.mask_fraction_file, agg_count.shape[1], agg_count.shape[0],
+                                       agg_count.shape[2], gdal.GDT_Float32, options=['INTERLEAVE=BIL'])
+            outDataset.SetProjection(abun_gdal.GetProjection())
+            outtrans = list(abun_gdal.GetGeoTransform())
+            outtrans[1] = args.resolution
+            outtrans[5] = -1*args.resolution
+            outDataset.SetGeoTransform(outtrans)
+            for _b in range(1, agg_count.shape[2]+1):
+                outDataset.GetRasterBand(_b).SetNoDataValue(-9999)
+                if band_names is not None:
+                    outDataset.GetRasterBand(_b).SetDescription(list(counts.keys())[_b-1])
+            del outDataset
+            _write_bil_chunk(agg_count.transpose((0,2,1)), args.mask_fraction_file, 0, (agg_count.shape[0], agg_count.shape[2], agg_count.shape[1]))
 
         # Now uncertainty
         if do_uncert:
